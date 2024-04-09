18efc13b6e50355b588b9b426b339a3d20473bde
unknown <grupoalev345@gmail.com>
2024-04-09 14:30:01 -0300

    2.0

---
+++ data/index.js
@@ -0,0 +1,47 @@
+const DATA =
+[
+    {
+        id:'01',
+        porcentagem:'28%',
+        titulo:'Leitura',
+        imagem:'https://img.freepik.com/vetores-gratis/pilha-de-livros-de-design-plano-desenhado-a-mao_23-2149334862.jpg',
+        descricao:'Todos os dias indo e voltando da escola eu leio'
+    },
+
+    {   id:'02',
+        porcentagem:'28%',
+        titulo:'Ir Para a Escola',
+        imagem:'https://www.creativefabrica.com/wp-content/uploads/2022/12/31/Kawaii-School-Yellow-Bus-Cartoon-55505931-1.png',
+        descricao:'Meu percurso até a escola'
+    },
+
+    {   id:'03',
+    porcentagem:'28%',
+    titulo:'Estudar',
+    imagem:'https://static.vecteezy.com/ti/vetor-gratis/p3/18807788-criancas-estudando-no-de-desenhos-animados-de-mesa-vetor.jpg',
+    descricao:'Eu estudo para o Enem'
+    },
+
+    {   id:'04',
+    porcentagem:'28%',
+    titulo:'Atividade',
+    imagem:'https://static.vecteezy.com/ti/vetor-gratis/p3/2290099-uma-garota-fazendo-licao-de-casa-doodle-personagem-de-desenho-animado-gratis-vetor.jpg',
+    descricao:'Eu faço as atividades escolares'
+    },
+
+    {   id:'05',
+    porcentagem:'28%',
+    titulo:'Jantar',
+    imagem:'https://static.vecteezy.com/ti/vetor-gratis/p1/1847431-comida-jantar-menu-cartoon-fresco-bife-bife-e-ovos-fritos-no-prato-gratis-vetor.jpg',
+    descricao:'Todos os dias quando eu chego da escola, eu janto'
+    },
+
+    {   id:'06',
+    porcentagem:'28%',
+    titulo:'Almoçar',
+    imagem:'https://static.vecteezy.com/ti/vetor-gratis/p1/1847431-comida-jantar-menu-cartoon-fresco-bife-bife-e-ovos-fritos-no-prato-gratis-vetor.jpg',
+    descricao:'Todos os dias antes de ir a escola, eu almoço'
+    },
+]
+
+export default DATA;
